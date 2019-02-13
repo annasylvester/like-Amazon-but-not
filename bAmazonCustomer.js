f0daa857d90613b1b6ce5d25aef9ead93f8c5f46
annasylvester <annaleesylvester@gmail.com>
2019-02-13 15:58:36 -0500

    Choices added

--- bAmazonCustomer.js
+++ bAmazonCustomer.js
@@ -13,7 +13,7 @@ let connection = mysql.createConnection({
 connection.connect(function (err) {
 	if (err) throw err;
 	console.log("connected as id " + connection.threadId);
-	buyProduct();
+	startApp();
 });
 
 // ENDS CONNECTION TO MYSQL
@@ -28,139 +28,101 @@ function showAllProducts() {
 // INQUIRER
 let inquirer = require('inquirer');
 
+// Empty shopping cart
+let shoppingCart = [];
 
+function startApp() {
+	inquirer
+	.prompt({
+		name: "whatToDo",
+		type: "list",
+		message: "What would you like to do?",
+		choices: ["BUY", "VIEW CART", "EXIT"]
+	})
+	.then(function(answer) {
+		if (answer.whatToDo === "BUY") {
+			buyProduct();
+		}
+		else if (answer.whatToDo === "VIEW CART") {
+			viewCart();
+		} else {
+			connection.end();
+		}
+	});
+}
 
-//     connection.query(
-//         "UPDATE products SET ? WHERE ?",
-
-//         [{
-//                 stock_quantity: newQuantity
-//             },
-
-//             {
-
-//             }
-//         ],
-
-//         function (error) {
-//             if (error) throw err;
-//             console.log("Bid placed successfully!");
-//             start();
-//         }
-//     );
-
-
-
-// } else {
-//     // not enough stock available message...
-//     console.log("There isn't enough stock of that product avaiable.");
-//     buyProduct();
-// }
-
-////////
-
-
-//     // bid was high enough, so update db, let the user know, and start over
-//     connection.query(
-//         "UPDATE auctions SET ? WHERE ?",
-//         [{
-//                 highest_bid: answer.bid
-//             },
-//             {
-//                 id: chosenItem.id
-//             }
-//         ],
-//         function (error) {
-//             if (error) throw err;
-//             console.log("Bid placed successfully!");
-//             start();
-//         }
-//     );
-// } else {
-//     // bid wasn't high enough, so apologize and start over
-//     console.log("Your bid was too low. Try again...");
-//     start();
-// }
-// 			});
-// 	});
-// }
-
+// Buy Product Function
 function buyProduct() {
 
 	connection.query("SELECT * FROM products", function (err, results) {
-			if (err) throw err;
+		if (err) throw err;
 
-			// Shows all products
-			console.log(results);
+		// Shows all products
+		console.log(results);
 
-			// User chooses item to buy
-			inquirer
-				.prompt([{
-					name: "choice",
-					type: "rawlist",
-					choices: function () {
-						let choiceArray = [];
-						for (let i = 0; i < results.length; i++) {
-							choiceArray.push(results[i].product_name);
+		// User chooses item to buy
+		inquirer
+			.prompt([{
+				name: "choice",
+				type: "rawlist",
+				choices: function () {
+					let choiceArray = [];
+					for (let i = 0; i < results.length; i++) {
+						choiceArray.push(results[i].product_name);
+					}
+					return choiceArray;
+				},
+				message: "What would you like to purchase?",
+			}])
+
+			.then(function (answer) {
+
+				for (let i = 0; i < results.length; i++) {
+					if (results[i].product_name === answer.choice) {
+						chosenItem = results[i];
+					}
+				}
+				console.log(chosenItem);
+
+				// Choose how many to buy
+				inquirer
+					.prompt([{
+						name: "howmany",
+						type: "input",
+						message: "How many would you like to purchase?"
+					}])
+
+					// if there is enough product, it reduces the quantity in MySQL
+					.then(function (answer) {
+						if (answer.howmany <= chosenItem.stock_quantity) {
+
+							let newQuantity = chosenItem.stock_quantity - answer.howmany;
+
+							connection.query(
+								"UPDATE products SET ? WHERE ?",
+								[{
+										stock_quantity: newQuantity
+									},
+									{
+										id: chosenItem.id
+									}
+								],
+								function (error) {
+									if (error) throw err;
+									console.log("Product added to shopping cart.");
+									
+									viewCart();
+								}
+							);
+						} else {
+							console.log("We're sorry. There is not enough stock available.");
 						}
-						return choiceArray;
-					},
-					message: "What would you like to purchase?",
-				}])
-
-				.then(function (answer) {
-
-						for (let i = 0; i < results.length; i++) {
-							if (results[i].product_name === answer.choice) {
-								chosenItem = results[i];
-							}
-						}
-						console.log(chosenItem);
-
-						inquirer
-							.prompt([{
-								name: "howmany",
-								type: "input",
-								message: "How many would you like to purchase?"
-							}])
-
-							.then(function (answer) {
-									if (answer.howmany <= chosenItem.stock_quantity) {
-										console.log("whoohoo theres enough stock");
-
-										let newQuantity = chosenItem.stock_quantity - answer.howmany;
-										console.log("new Quantity created");
-										console.log(newQuantity);
-									};
-
-								// 	connection.query("UPDATE products SET ? WHERE ?",
-
-								// 		[{
-								// 				stock_quantity: newQuantity
-								// 			},
-
-								// 			{
-
-								// 			}
-								// 		],
-
-								// 		function (error) {
-								// 			if (error) throw err;
-								// 			console.log("Bid placed successfully!");
-								// 			start();
-								// 		}
-								// 	);
-
-
-
-								// } else {
-								// 	// not enough stock available message...
-								// 	console.log("There isn't enough stock of that product avaiable.");
-								// 	buyProduct();
-								// }
-
-
-							});
-				});
+					});
+			});
 	});
 };
+
+function viewCart() {
+
+}
+
